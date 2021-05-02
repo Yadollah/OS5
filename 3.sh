2b9fdc725f33cf0f2bed77bd425d510962f87e5e
Yadollah Habibkazemi <yadollahhabib@gmail.com>
2021-05-02 09:44:03 -0700

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,7 +1,7 @@
 cd test
 git init
 git add --all
-git commit -m "testing"
+git commit -m "test"
 git remote add origin https://github.com/Yadollah/test.git
 git branch -M main
 git push -u origin main
